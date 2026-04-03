5151c735f831483a75f3f3d7783f8c78389b0617
santiagohenrique <henrique11santiagopires@hotmail.com>
2026-04-03 00:41:34 -0300

    feat: add eslint and prettier to improve code standards and maintenance

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'no-console': 'off',
+    },
+  },
+  eslintConfigPrettier,
+)
