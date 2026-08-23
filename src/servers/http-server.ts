6fd3a7a0845f70dabebc5b660f3a121579711b05
santiagohenrique <henrique11santiagopires@hotmail.com>
2026-08-23 15:38:49 -0300

    feat: add express-rate-limit middleware for request throttling

--- src/servers/http-server.ts
+++ src/servers/http-server.ts
@@ -1,9 +1,18 @@
 import express from 'express'
 import { createServer } from 'http'
 import { registerContextRoutes } from '../contexts/routes'
+import { rateLimit } from 'express-rate-limit'
 
 const app = express()
 
+// in-memory rate limit
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 15,
+  message: 'Too many requests from this IP, please try again later'
+});
+
+app.use(limiter)
 app.use(express.json())
 registerContextRoutes(app)
 
